0dc1840b2ffeed0fd9aca4a3aab544770c9fba3a
Ariel Chen <happilyeverafter0800@gmail.com>
2025-01-16 19:05:15 -0500

    finish lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,40 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(f"{i}")
+        i -= 1
+
+    print("Happy New Year!")
+
+        
+    
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    # num_square = num * num
+    #loop through [1,2,3,4,5]
+    squared_list = []
+    for integer in int_list:
+        squared = integer * integer
+        squared_list.append(squared)
+    return squared_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    #fizz = num % 3
+    #buzz = num % 5
+    #fizzbuzz = num % 3 and 5
+    
+    for num in range(1, 101):
+        if num % 5 == 0 and num % 3 == 0:
+            print("FizzBuzz")
+        elif num % 5 == 0:
+            print("Buzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        else:
+            print(num)
+
+
